3f3aff7bb02cbd596562bc55a146fe1f76de5574
Roman <moywainua@gmail.com>
2017-09-01 02:39:01 +0300

    Solve problems with memory using ipython magic commands

--- src/final.py
+++ src/final.py
@@ -5,7 +5,6 @@ Created on Mon May 29 23:48:38 2017
 
 @author: roman
 """
-
 import pandas as pd
 import numpy as np
 import gc
@@ -13,6 +12,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn import linear_model
 from func import toCategorical, solveNA, Dummies, solveCategorical, moreFeautures
 import os
+from IPython import get_ipython
+ipython = get_ipython()
 
 #chanhe this manualy
 path = '/media/roman/Main/Programing/contest/dmc2017/dmc-2017/'
@@ -21,11 +22,15 @@ os.chdir(path)
 items = pd.read_csv('data/raw/items.csv',sep='|')
 train = pd.read_csv('data/raw/train.csv',sep='|')
 
-
-
 train_items=pd.merge(train,items,on='pid')
+
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 del train
 gc.collect
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
+
 train_items=toCategorical(train_items)
 
 
@@ -39,14 +44,10 @@ train_items=solveNA(train_items,train_items,coef_competitorPrice_to_rrp,0)
 train_items=Dummies(train_items)
 train_items=moreFeautures(train_items)
 
-#items = solveCategorical('revenue',train_items,items,1)
+items = solveCategorical('revenue',train_items,items,1)
 #items = solveCategorical('click',train_items,items,0)
 #items = solveCategorical('basket',train_items,items,0)
 #items = solveCategorical('order',train_items,items,0)
-#items.to_csv('items_vx')
-
-
-items = pd.read_csv('data/interim/items_v1.csv')
 
 items_pred=list(items.columns)
 t1=['pid']
@@ -154,28 +155,48 @@ predictors=[ #'lineID',
 y_train = train_items['revenue']
 x_train = train_items[predictors]
 
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 del train_items
 gc.collect
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 
 scaler = MinMaxScaler()
 x_train=scaler.fit_transform(x_train)
+
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 
 model_lasso = linear_model.LassoCV(alphas = [1, 0.16, 0.1, 0.001, 0.0005]).fit(x_train, y_train)
 
 model_ridge = linear_model.Ridge(alpha=6, fit_intercept=True, max_iter=10000)
 model_ridge.fit(x_train, y_train)
 
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 del y_train,x_train
 gc.collect
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 
-items = pd.read_csv('data/raw/items.csv',sep='|')
+items_init = ['pid',
+              'manufacturer',
+              'group',
+              'content',
+              'unit',
+              'pharmForm',
+              'genericProduct',
+              'salesIndex',
+              'category',
+              'campaignIndex',
+              'rrp']
 clas = pd.read_csv('data/raw/class.csv',sep='|')
-clas_items=pd.merge(clas,items,on='pid')
+clas_items=pd.merge(clas,items[items_init],on='pid')
 clas_items=toCategorical(clas_items)
 clas_items=solveNA(clas_items,clas_items,coef_competitorPrice_to_rrp,0)
 clas_items=Dummies(clas_items)
 clas_items=moreFeautures(clas_items)
-items = pd.read_csv('data/interim/items_v1.csv')
 clas_items=pd.merge(clas_items,items[items_pred],on='pid')
 
 submission = pd.DataFrame({
@@ -184,8 +205,13 @@ submission = pd.DataFrame({
 })
 
 x_test = clas_items[predictors]
+
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 del clas_items,clas,items
 gc.collect
+ipython.magic("%reset -f out")
+ipython.magic("%reset -f in")
 
 x_test = x_test.fillna(x_test.mean())
 x_test = scaler.fit_transform(x_test)
